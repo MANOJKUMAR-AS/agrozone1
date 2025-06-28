fc6eae5e23db975646e9d515f3b4afa1cfafaaf6
MANOJKUMAR-AS <mk19895349@gmail.com>
2025-06-28 12:17:39 +0530

    Delete src directory

--- src/pages/about.js
+++
@@ -1,9 +0,0 @@
-// about.js
-export default function About() {
-  return (
-    <div>
-      <h1>About Agrozone</h1>
-      <p>This app provides smart crop suggestions tailored to your region.</p>
-    </div>
-  );
-}
